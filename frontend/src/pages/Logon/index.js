6dfc9783429e9e8d5d0f97177e4e0c0aab19bba0
Paty Alves <patriciaalves10@gmail.com>
2020-03-26 22:52:05 -0300

    logon and register page

---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiLogIn } from 'react-icons/fi';
+
+import './styles.css';
+
+import heroesImg from '../../assets/heroes.png';
+import logoImg from '../../assets/logo.svg';
+
+const Logon = props => {
+  return (
+    <div className='logon-container'>
+      <section className='form'>
+        <img src={logoImg} />
+
+        <form>
+          <h1>Faça seu logon</h1>
+          <input placeholder='sua ID' />
+          <button type='submit' className='button'>
+            Entrar
+          </button>
+          <Link to='/register' className='back-link'>
+            <FiLogIn size={16} color='#E02041' />
+            Não tenho cadastro
+          </Link>
+        </form>
+      </section>
+      <img src={heroesImg} alt='heroes' />
+    </div>
+  );
+};
+
+export default Logon;
